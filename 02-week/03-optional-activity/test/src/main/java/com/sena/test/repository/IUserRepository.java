4393dcf9005eaef09aed3582743dfcb24f9d4d51
danna barrios <dannabarrios0820@gmail.com>
2026-03-10 14:24:48 -0500

    Estructura final corregida

--- 02-week/03-optional-activity/test/src/main/java/com/sena/test/repository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.test.repository;
-
-import com.sena.test.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
