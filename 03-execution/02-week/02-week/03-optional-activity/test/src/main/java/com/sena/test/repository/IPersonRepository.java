54bf8b497e3ff63ce226212629bfadedc57d5697
danna barrios <dannabarrios0820@gmail.com>
2026-03-10 15:49:17 -0500

    Estructura final semanas en 03-execution

--- 03-execution/02-week/02-week/03-optional-activity/test/src/main/java/com/sena/test/repository/IPersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.test.repository;
-
-import com.sena.test.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPersonRepository extends JpaRepository<Person, Long> {
-}
--- 03-execution/02-week/02-week/03-optional-activity/test/src/main/java/com/sena/test/repository/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.test.repository;
-
-import com.sena.test.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long> {
-}
